33792aabc2b344f113f4e030afefc7f1326ae60d
wataruoguchi <wataru.oguchi.0to1@gmail.com>
2015-02-18 11:56:56 -0800

    test first
    have built multiply with Test first method

--- sample/js/maths.js
+++ sample/js/maths.js
@@ -1,13 +1,22 @@
 /*global module*/
 
-var add = function (numA, numB) {
+var _add = function (numA, numB) {
 	return numA + numB;
 };
-var subtract = function (numA, numB) {
+var _subtract = function (numA, numB) {
 	return numA - numB;
+};
+var _multiply = function (numA, numB) {
+	var hasArgs = !(numA === undefined || numB == undefined),
+		isNumbers = (typeof numA === "number" && typeof numB === "number");
+	if(!hasArgs || !isNumbers) {
+		return null;
+	}
+	return numA * numB;
 };
 
 if (typeof module !== 'undefined' && typeof module.exports !== 'undefined') {
-	module.exports.add = add;
-	module.exports.subtract = subtract;
+	module.exports.add = _add;
+	module.exports.subtract = _subtract;
+	module.exports.multiply = _multiply;
 }
--- sample/test/maths.js
+++ sample/test/maths.js
@@ -12,4 +12,30 @@ describe('Mathematical formula', function(){
 			expect(maths.add(1,-20)).to.be.lessThan(1);
 		});
 	});
+	describe('Subtraction', function(){
+		it('should pass with positive values', function(){
+			expect(maths.subtract(1,2)).to.be(-1);
+			expect(maths.subtract(2,1)).to.be(1);
+			expect(maths.subtract(0,1)).to.be(-1);
+			expect(maths.subtract(1,0)).to.be(1);
+		});
+		it('should pass with negative values', function(){
+			expect(maths.subtract(-1,2)).to.be(-3);
+			expect(maths.subtract(-2,1)).to.be(-3);
+			expect(maths.subtract(-1,0)).to.be(-1);
+			expect(maths.subtract(1,2)).to.be(-1);
+			expect(maths.subtract(2,1)).to.be(1);
+			expect(maths.subtract(0,-1)).to.be(1);
+		});
+	});
+	describe('Multiply', function(){
+		it('should pass with positive values', function(){
+			expect(maths.multiply(0,1)).to.be(0);
+		});
+		it('should fail with error', function(){
+			expect(maths.multiply(0)).to.be(null);
+			expect(maths.multiply({},1)).to.be(null);
+			expect(maths.multiply("aa",1)).to.be(null);
+		});
+	});
 });
