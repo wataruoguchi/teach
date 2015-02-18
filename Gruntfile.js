528f41e2b7424fcacd8af56c7a0fd25196d499e9
Dan BROOKS <githubcom@domaindesign.ca>
2015-02-18 07:04:12 -0800

    Remove

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,20 +4,7 @@ module.exports = function(grunt) {
 
 	// Project configuration.
 	grunt.initConfig({
-		concat: {
-			dev: {
-				src: ["sample/js/*.js"],
-				dest: 'dist/built.js',
-			}
-		},
 		pkg: grunt.file.readJSON('package.json'),
-		uglify: {
-			dev: {
-				files: {
-					'dist/built.min.js': ['dist/built.js']
-				}
-			}
-		},
 		watch: {
 			concat: {
 				files: '<%= concat.dev.src %>',
@@ -28,9 +15,7 @@ module.exports = function(grunt) {
 
 	// Load Grunt.js plugins
 	grunt.loadNpmTasks('grunt-contrib-watch');
-	grunt.loadNpmTasks('grunt-contrib-concat');
-	grunt.loadNpmTasks('grunt-contrib-uglify');
 
 	// Task(s).
-	grunt.registerTask('bundle', ['concat', 'uglify']);
+	//grunt.registerTask('bundle', ['concat', 'uglify']);
 };
